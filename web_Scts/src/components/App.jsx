d68e011fd456f691ce59b31741092abd3fb03242
Nikita <bletigri@gmail.com>
2024-09-27 15:55:59 +0300

    update data

--- web_Scts/src/components/App.jsx
+++ web_Scts/src/components/App.jsx
@@ -34,13 +34,7 @@ export default function App() {
     return (
     <>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-		<h5 class="my-0 mr-md-auto">itProger</h5>
-		<nav class="my-2 my-md-0 mr-md-3">
-			<a class="p-2 text-dark" href="#">Главная</a>
-			<a class="p-2 text-dark" href="#">Про нас</a>
-			<a class="p-2 text-dark" href="#">Поддержка</a>
-			<a class="p-2 text-dark" href="#">Цены</a>
-		</nav>
+		<h5 class="my-0 mr-md-auto">zxc The Self</h5>
 		<a class="btn btn-outline-primary" href="#">Регистрация</a>
 	</div>
 	<div class="container">
